fa8ec8b6abc3e82acbcdec3336c6c31dba235cff
Emre COŞKUN <emrecoskun06@hotmail.com>
2023-01-07 16:55:02 +0300

    Mail servisi tamamlandı.

---
+++ Core/ECommerce.Application/Abstractions/Services/IMailService.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerce.Application.Abstractions.Services
+{
+    public interface IMailService
+    {
+        Task SendMessageAsync(string to,string subject, string body, bool isBodtHtml = true);
+        Task SendMessageAsync(string[] to, string subject, string body, bool isBodyHtml = true);
+
+    }
+}
