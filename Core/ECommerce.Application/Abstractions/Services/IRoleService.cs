d3cf1a732d651b02d6e504a8e0604c52dd1f12a5
Emre COŞKUN <emrecoskun06@hotmail.com>
2023-01-16 00:27:10 +0300

    Roller için rol servisi oluşturuldu, ilgili cqrs yapılanması yapıldı ve migration edildi. Role ve authorizationendpoint controller ı yazıldı

--- Core/ECommerce.Application/Abstractions/Services/IRoleService.cs
+++ Core/ECommerce.Application/Abstractions/Services/IRoleService.cs
@@ -4,8 +4,10 @@ namespace ECommerce.Application.Abstractions.Services
 {
     public interface IRoleService
     {
+        (object, int) GetAllRoles(int page, int size);
+        Task<(string id, string name)> GetRoleById(string id); 
         Task<bool> CreateRoleAsync(string name);
-        Task<bool> DeleteRoleAsync(string name);
+        Task<bool> DeleteRoleAsync(string id);
         Task<bool> UpdateAsync(string id, string name);
     }
 }
